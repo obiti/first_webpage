e890d5f20a453e1cc1ac63ba834ab9f5384c987d
obiti <62120211+obiti@users.noreply.github.com>
2020-03-26 23:05:24 +0100

    Webpage
    HTML, CSS and JAVASCRIPT

---
+++ script.js
@@ -0,0 +1,31 @@
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<title>Fisrt Web Page </title>
+	</head>
+	<body>
+	<p>Kindly do exactly what you see here as it would affect your score <br>
+	Whether it is your first webpage in life or not, use the heading above it won't bite..</p>
+	<script>
+	document.getElemetById("demo").style.backgroundColor = "yellow";
+	<h1>H2 Heading</h1>
+	<p>As a best practice, your webpage should have just one</p>
+	<p>element</p>
+	<h2> h3 Heading</h2>
+	<p>This test is too simple to fail.<br>
+	It is simply a test of how much you have been following the course.</p>
+	<p>Thanks.. Wait..<br>
+	You will have to upload this project to github..Yea.</p>
+	<h3>H4 Heading</h3>
+	<p>Use the file structure that will be shown here below</p>
+	<p>:::first_webpage:::<br>
+ |<br>
+-------- :::index.html:::<br>
+ |<br>
+-------- :::style.css:::<br>
+ |<br>
+-------- :::script.js:::</p>
+	</body>
+	</html>
+
+	
